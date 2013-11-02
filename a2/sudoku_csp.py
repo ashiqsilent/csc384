f6ecf6afad1a6e2314b3975c3676099f93ac7592
ashiqsilent <ashiq.silent@gmail.com>
2013-11-01 22:39:20 -0700

    added

---
+++ a2/sudoku_csp.py
@@ -0,0 +1,122 @@
+from cspbase import *
+
+
+def enforce_gac(constraint_list):
+    '''Input a list of constraint objects, each representing a constraint, then 
+       enforce GAC on them pruning values from the variables in the scope of
+       these constraints. Return False if a DWO is detected. Otherwise, return True. 
+       The pruned values will be removed from the variable object cur_domain. 
+       enforce_gac modifies the variable objects that are in the scope of
+       the constraints passed to it.'''
+
+#<<<your implemenation of enforce_gac below
+
+#>>>your implemenation of enforce_gac above
+
+                            
+def sudoku_enforce_gac_model_1(initial_sudoku_board):
+    '''The input board is specified as a list of 9 lists. Each of the
+       9 lists represents a row of the board. If a 0 is in the list it
+       represents an empty cell. Otherwise if a number between 1--9 is
+       in the list then this represents a pre-set board
+       position. E.g., the board
+    
+       -------------------  
+       | | |2| |9| | |6| |
+       | |4| | | |1| | |8|
+       | |7| |4|2| | | |3|
+       |5| | | | | |3| | |
+       | | |1| |6| |5| | |
+       | | |3| | | | | |6|
+       |1| | | |5|7| |4| |
+       |6| | |9| | | |2| |
+       | |2| | |8| |1| | |
+       -------------------
+       would be represented by the list of lists
+       
+       [[0,0,2,0,9,0,0,6,0],
+       [0,4,0,0,0,1,0,0,8],
+       [0,7,0,4,2,0,0,0,3],
+       [5,0,0,0,0,0,3,0,0],
+       [0,0,1,0,6,0,5,0,0],
+       [0,0,3,0,0,0,0,0,6],
+       [1,0,0,0,5,7,0,4,0],
+       [6,0,0,9,0,0,0,2,0],
+       [0,2,0,0,8,0,1,0,0]]
+       
+       
+       In model_1 you should create a variable for each cell of the
+       board, with domain equal to {1-9} if the board has a 0 at that
+       position, and domain equal {i} if the board has a fixed number i
+       at that cell. 
+       
+       Model_1 should create BINARY CONSTRAINTS OF NOT-EQUAL between all
+       relevant variables (e.g., all pairs of variables in the same
+       row), then invoke enforce_gac on those constraints. All of the
+       constraints of Model_1 MUST BE binary constraints (i.e.,
+       constraints whose scope includes two and only two variables).
+       
+       This function outputs the GAC consistent domains after
+       enforce_gac has been run. The output is a list with the same
+       layout as the input list: a list of nine lists each
+       representing a row of the board. However, now the numbers in
+       the positions of the input list are to be replaced by LISTS
+       which are the corresponding cell's pruned domain (current
+       domain) AFTER gac has been performed.
+       
+       For example, if GAC failed to prune any values the output from
+       the above input would result in an output would be: NOTE I HAVE
+       PADDED OUT ALL OF THE LISTS WITH BLANKS SO THAT THEY LINE UP IN
+       TO COLUMNS. Python would not output this list of list in this
+       format.
+       
+       
+       [[[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                2],[1,2,3,4,5,6,7,8,9],[                9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                6],[1,2,3,4,5,6,7,8,9]],
+       [[1,2,3,4,5,6,7,8,9],[                4],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                1],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                8]],
+       [[1,2,3,4,5,6,7,8,9],[                7],[1,2,3,4,5,6,7,8,9],[                4],[                2],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                3]],
+       [[                5],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                3],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]],
+       [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                1],[1,2,3,4,5,6,7,8,9],[                6],[1,2,3,4,5,6,7,8,9],[                5],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]],
+       [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                3],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                6]],
+       [[                1],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                5],[                7],[1,2,3,4,5,6,7,8,9],[                4],[1,2,3,4,5,6,7,8,9]],
+       [[                6],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                2],[1,2,3,4,5,6,7,8,9]],
+       [[1,2,3,4,5,6,7,8,9],[                2],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                8],[1,2,3,4,5,6,7,8,9],[                1],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]]
+       
+       Of course, GAC would prune some variable domains so this would
+       not be the outputted list.
+
+    '''
+
+#<<<your implemenation of model_1  below
+
+#>>>your implemenation of model_1 above
+
+
+
+##############################
+
+def sudoku_enforce_gac_model_2(initial_sudoku_board):
+    '''This function takes the same input format (a list of 9 lists
+    specifying the board, and generates the same format output as
+    sudoku_enforce_gac_model_1.
+    
+    The variables of model_2 are the same as for model_1: a variable
+    for each cell of the board, with domain equal to {1-9} if the
+    board has a 0 at that position, and domain equal {i} if the board
+    has a fixed number i at that cell.
+
+    However, model_2 has different constraints. In particular, instead
+    of binary non-equals constaints model_2 has 27 all-different
+    constraints: all-different constraints for the variables in each
+    of the 9 rows, 9 columns, and 9 sub-squares. Each of these
+    constraints is over 9-variables (some of these variables will have
+    a single value in their domain). model_2 should create these
+    all-different constraints between the relevant variables,
+    invoke enforce_gac on those constraints, and then output the list of gac 
+    consistent variable domains in the same format as for model_1.
+    '''
+     
+    
+#<<<your implemenation of model_2  below
+
+#>>>your implemenation of model_2 above
+
