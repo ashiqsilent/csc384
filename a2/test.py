0c88a62bc33e53f1bc66d524b9779d64e4ddf0d8
ashiqsilent <ashiq.silent@gmail.com>
2013-11-04 13:25:11 -0800

    cons

--- a2/test.py
+++ a2/test.py
@@ -18,14 +18,24 @@ def create_variables(board):
 
 def create_constraint(variables):
     constraint_list = []
+    row = 0
+    col = 0
     
     return constraint_list
+
+def create_tuples(dom1, dom2):
+    tuples = []
+    for v1 in dom1:
+        for v2 in dom2:
+            if v1 != v2:
+                tuples.append([v1,v2])
+    return tuples
+
 
 board = [[0,0,2,0,9,0,0,6,0], [0,4,0,0,0,1,0,0,8], [0,7,0,4,2,0,0,0,3], [5,0,0,0,0,0,3,0,0], [0,0,1,0,6,0,5,0,0],  [0,0,3,0,0,0,0,0,6], [1,0,0,0,5,7,0,4,0], [6,0,0,9,0,0,0,2,0], [0,2,0,0,8,0,1,0,0]]
 
 var = create_variables(board)
 c = Constraint("C1", [var[0][0], var[0][1]])
+c.add_satisfying_tuples(create_tuples(c.scope[0].domain(), c.scope[1].domain()))
 
-c.add_satisfying_tuples([[], []])
-c.add_satisfying_tuples([[2], []])
 c.print_constraint_all()
