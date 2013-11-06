4d32fac401a4d5632cb82a81fbf4a434afd79526
ashiqsilent <ashiq.silent@gmail.com>
2013-11-05 19:47:43 -0800

    created constraints list

--- a2/sudoku_csp.py
+++ a2/sudoku_csp.py
@@ -90,6 +90,7 @@ def sudoku_enforce_gac_model_1(initial_sudoku_board):
     # 9X9 square matrix representing the board
     variables = create_variables(initial_sudoku_board)
     constraints = create_constraints(variables)
+    
 #>>>your implemenation of model_1 above
 
 
@@ -137,3 +138,57 @@ def create_variables(board):
         variable_matrix.append(variable_row)
         i += 1
     return variable_matrix
+
+def create_tuples(dom1, dom2):
+    tuples = []
+    for v1 in dom1:
+        for v2 in dom2:
+            if v1 != v2:
+                tuples.append([v1,v2])
+    return tuples
+
+def extract_column(i, j, matrix):
+    col = [matrix[i][j]]
+    i += 1
+    while i < 9:
+        col.append(matrix[i][j])
+        i += 1
+    return col
+
+def extract_sub_square(i,j,matrix):
+    sub_square = [matrix[i][j]]
+    i += 1
+    while i%3 != 0:
+        col = j - j%3
+        while (col%3 != 0) or (col <= j):
+            if col != j:
+                sub_square.append(matrix[i][col])
+            col += 1
+        i += 1
+    return sub_square
+
+def add_binary_constraints(variables):
+    constraints = []
+    var1 = variables[0]
+    i = 1
+    while i < len(variables):
+        var2 = variables[i]
+        c = Constraint("C_{}{}".format(var1.name, var2.name), [var1, var2])
+        c.add_satisfying_tuples(create_tuples(c.scope[0].domain(), c.scope[1].domain()))
+        constraints.append(c)
+        i += 1
+    return constraints
+
+def create_binary_constraints(variables):
+    constraints = []
+    for row in variables:
+        for cell in row:
+            # create the row constraints
+            constraints += add_binary_constraints(row[row.index(cell):])
+            # create the column constraints
+            column = extract_column(variables.index(row), row.index(cell), variables)
+            constraints += add_binary_constraints(column)
+            # create the sub-square constraints
+            sub_square = extract_sub_square(variables.index(row), row.index(cell), variables)
+            constraints += add_binary_constraints(sub_square)
+    return constraints
--- a2/test.py
+++ a2/test.py
@@ -16,13 +16,6 @@ def create_variables(board):
         i += 1
     return variable_matrix
 
-def create_constraint(variables):
-    constraint_list = []
-    row = 0
-    col = 0
-    
-    return constraint_list
-
 def create_tuples(dom1, dom2):
     tuples = []
     for v1 in dom1:
@@ -31,11 +24,57 @@ def create_tuples(dom1, dom2):
                 tuples.append([v1,v2])
     return tuples
 
+def extract_column(i, j, matrix):
+    col = [matrix[i][j]]
+    i += 1
+    while i < 9:
+        col.append(matrix[i][j])
+        i += 1
+    return col
+
+def extract_sub_square(i,j,matrix):
+    sub_square = [matrix[i][j]]
+    i += 1
+    while i%3 != 0:
+        col = j - j%3
+        while (col%3 != 0) or (col <= j):
+            if col != j:
+                sub_square.append(matrix[i][col])
+            col += 1
+        i += 1
+    return sub_square
+
+def add_binary_constraints(variables):
+    constraints = []
+    var1 = variables[0]
+    i = 1
+    while i < len(variables):
+        var2 = variables[i]
+        c = Constraint("C_{}{}".format(var1.name, var2.name), [var1, var2])
+        c.add_satisfying_tuples(create_tuples(c.scope[0].domain(), c.scope[1].domain()))
+        constraints.append(c)
+        i += 1
+    return constraints
+
+def create_binary_constraints(variables):
+    constraints = []
+    for row in variables:
+        for cell in row:
+            # create the row constraints
+            constraints += add_binary_constraints(row[row.index(cell):])
+            # create the column constraints
+            column = extract_column(variables.index(row), row.index(cell), variables)
+            constraints += add_binary_constraints(column)
+            # create the sub-square constraints
+            sub_square = extract_sub_square(variables.index(row), row.index(cell), variables)
+            constraints += add_binary_constraints(sub_square)
+    return constraints
 
 board = [[0,0,2,0,9,0,0,6,0], [0,4,0,0,0,1,0,0,8], [0,7,0,4,2,0,0,0,3], [5,0,0,0,0,0,3,0,0], [0,0,1,0,6,0,5,0,0],  [0,0,3,0,0,0,0,0,6], [1,0,0,0,5,7,0,4,0], [6,0,0,9,0,0,0,2,0], [0,2,0,0,8,0,1,0,0]]
 
 var = create_variables(board)
-c = Constraint("C1", [var[0][0], var[0][1]])
-c.add_satisfying_tuples(create_tuples(c.scope[0].domain(), c.scope[1].domain()))
 
-c.print_constraint_all()
+cons = create_binary_constraints(var)
+
+for c in cons:
+    print c.name
